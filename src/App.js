b6717c44b88461721462b402fafcf5989d60249a
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2023-07-10 16:02:13 +0530

    git commit -m "first phase"

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Footer, Blog, Possibility, Features, Whatgpt, Header} from './containers';
+import { CTA, Brand, Navbar} from './components';
+
+import './App.css';
+
+const App = () => {
+return (
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar/>
+            <Header />
+        </div>
+        <Brand />
+        <Whatgpt />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+)
+}
+
+export default App
